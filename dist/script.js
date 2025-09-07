19ac81df7a793016031bdc00ed8d520bdbdfda5f
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2025-09-07 19:09:04 +0100

    configure typescript and set up initial project structure

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+"use strict";
+let message = "Test 1";
+let htmlElement = document.getElementById("message");
+if (htmlElement) {
+    htmlElement.innerText = message;
+}
---
+++ src/script.ts
@@ -0,0 +1,7 @@
+
+let message: string = "Hello World";
+let htmlElement = document.getElementById("message");
+if (htmlElement)
+{
+    htmlElement.innerText = message    
+}
